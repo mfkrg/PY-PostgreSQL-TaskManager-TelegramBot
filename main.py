1e728dbf0e11de83cc63ed7a0fe7f632d3d83e86
mfkrg <mfkrg@yandex.ru>
2022-08-17 00:49:02 +0300

    0.2 Добавлена регистрация с занесением в PostgreSQL Добавлена проверка наличия никнейма юзера в БД

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ connection = psycopg2.connect(
 )
 connection.autocommit = True
 cursor = connection.cursor()
+postgreSQL_select_Query = "select * from tasks"
+getnickname="select nickname from users"
 
 bot = telebot.TeleBot('5323448846:AAHhQHdJTTEH7uzNRhTjpt8vOYdhlv91KF0')
 
@@ -18,14 +20,29 @@ def start(message):
     startMessage = f'Привет, <b><u>{message.from_user.first_name}</u></b>. Для того что бы посмотреть все свои задачи напиши /tasks. Если ты хочешь добавить свою задачу напиши /addtask.'
     bot.send_message(message.chat.id, startMessage, parse_mode='html')
 
-@bot.message_handler()
+@bot.message_handler(commands=['register'])
+def registration(message):
+    current_user_nickname = message.from_user.username
+    current_user_first_name = message.from_user.first_name
+    cursor.execute(f"SELECT COUNT(*) FROM users WHERE nickname = '{current_user_nickname}'")
+
+    res = cursor.fetchall()[0][0]
+    if res == 0:
+        print(res)
+        print("can")
+        bot.send_message(message.chat.id, "Ого, тебя нет в списке!\nСейчас я занесу тебя в базу данных, не волнуйся\nПодожди пару секунд и отправь команду еще раз.")
+        cursor.execute(f'''INSERT INTO users (first_name, nickname) VALUES ('{current_user_first_name}', '{current_user_nickname}');''')
+
+    else:
+        print(res)
+        print("already")
+        bot.send_message(message.chat.id, "Сейчас ты в базе данных.")
+@bot.message_handler(commands=['tasks'])
 def get_user_text(message):
-    if message.text == "/tasks":
-        postgreSQL_select_Query = "select * from tasks"
-        cursor.execute(postgreSQL_select_Query)
-        users_records = cursor.fetchall()
-        text = '\n\n'.join([', '.join(map(str, x)) for x in users_records])
-        bot.send_message(message.chat.id, (str(text)))
+    cursor.execute(postgreSQL_select_Query)
+    users_records = cursor.fetchall()
+    text = '\n\n'.join([', '.join(map(str, x)) for x in users_records])
+    bot.send_message(message.chat.id, (str(text)))
 
 
 
